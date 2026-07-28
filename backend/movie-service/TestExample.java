08bc8b980d7b0e63b3f7e99e025e1860ade11932
Liviu Ionesi <pehlivanu@gmail.com>
2026-07-28 21:13:25 +0200

    chore: remove unused TestExample.java file (#80)

--- backend/movie-service/TestExample.java
+++
@@ -1,8 +0,0 @@
-import com.filmpire.movie.model.Movie;
-import org.springframework.data.mongodb.core.query.Criteria;
-public class TestExample {
-    public static void main(String[] args) {
-        Movie m = new Movie();
-        Criteria c = Criteria.byExample(m);
-    }
-}
